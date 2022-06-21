8562bacfc0b74e73df41f006c79ec92504d801d7
Nahuel Correa <nec2multivackergithub@outlook.com>
2022-06-21 20:08:03 -0300

    user class ereased

--- blog/src/main/java/com/nec2dev/blog/BlogApplication.java
+++ blog/src/main/java/com/nec2dev/blog/BlogApplication.java
@@ -5,38 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BlogApplication {
-	private String userName="";
-	private String name="";
-	private String lastName="";
-	
-	public void BlogAplication() {
-		
-	}
-	public String getUserName() {
-		return userName;
-	}
-
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
