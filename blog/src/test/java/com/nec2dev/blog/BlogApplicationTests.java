e9950a391ca7c3d28f4991dfea76aed561f73d99
Nahuel <71109200+nec2dev@users.noreply.github.com>
2022-06-21 19:38:49 -0300

    Spring blog foundation

---
+++ blog/src/test/java/com/nec2dev/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nec2dev.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
